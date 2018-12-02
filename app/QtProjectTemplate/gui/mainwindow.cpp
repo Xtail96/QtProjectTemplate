bb288e0273ad4e050b9298c02836cbf9ebc22ed0
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-12-02 16:02:41 +0300

    Подключены модели и доработано оформление statusBar

--- app/QtProjectTemplate/gui/mainwindow.cpp
+++ app/QtProjectTemplate/gui/mainwindow.cpp
@@ -6,6 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->mainToolBar->hide();
+
+    Router& router = Router::getInstance();
+    ui->label->setText(router.getRepository()->helloString());
+
+    // установка оформления statusBar
+    ui->statusBar->setStyleSheet("background-color: #333; color: #33bb33");
+    ui->statusBar->setFont(QFont("Consolas", 14));
+    ui->statusBar->showMessage(tr("State: ready 0123456789"));
 }
 
 MainWindow::~MainWindow()
